2dcdc56b18bdab445aa1db4e9e4f94b8ac984995
hisomura <hisomura.noreply@gmail.com>
2020-07-04 16:21:49 +0900

    Progress almost up to step 6.
    Some bags and type errors remain.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import './index.css'
 import Didact from './lib/diadact'
 
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
-)
-console.log('hello, world')
-console.log(element)
+// const element = (
+//   <div id="foo">
+//     <a>bar</a>
+//     <b />
+//   </div>
+// )
+// console.log('hello, world')
+// console.log(element)
+//
+// const container = document.getElementById("root")
+// if (!container) throw new Error('no root.')
+// Didact.render(element, container)
 
 const container = document.getElementById("root")
-if (!container) throw new Error('no root.')
-Didact.render(element, container)
+
+// @ts-ignore
+const updateValue = e => {
+  rerender(e.target!.value)
+}
+
+// @ts-ignore
+const rerender = value => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  )
+  Didact.render(element, container!)
+}
+
+rerender("World")
--- src/lib/diadact.ts
+++ src/lib/diadact.ts
@@ -7,11 +7,15 @@ type Fiber = {
   parent: Fiber | null
   child?: Fiber
   sibling?: Fiber
+  alternate?: Fiber | null
+  effectTag?: 'UPDATE' | 'PLACEMENT' | 'DELETION'
 }
 
 // FIXME
 let nextUnitOfWork: Fiber | null = null
 let wipRoot: Fiber | null = null
+let currentRoot: Fiber | null = null
+let deletions: Fiber[] | null = null
 window.requestIdleCallback(workLoop)
 
 function workLoop(deadline: RequestIdleCallbackDeadline) {
@@ -34,26 +38,7 @@ function performUnitOfWork(fiber: Fiber): Fiber | null {
   }
 
   if ('children' in fiber.props) {
-    const elements = fiber.props.children
-    let prevSibling: Fiber | null = null
-
-    for (let i = 0; i < elements.length; i += 1) {
-      const element = elements[i]
-      const newFiber: Fiber = {
-        type: element.type,
-        parent: fiber,
-        dom: null,
-        props: element.props,
-      }
-
-      if (!prevSibling) {
-        fiber.child = newFiber
-      } else {
-        prevSibling.sibling = newFiber
-      }
-
-      prevSibling = newFiber
-    }
+    reconcileChildren(fiber, fiber.props.children)
   }
 
   if (fiber.child) {
@@ -70,6 +55,56 @@ function performUnitOfWork(fiber: Fiber): Fiber | null {
   return null
 }
 
+function reconcileChildren(wipFiber: Fiber, elements: VirtualNode[]) {
+  let oldFiber = wipFiber.alternate?.child
+  let prevSibling: Fiber | null = null
+
+  for (let i = 0; i < elements.length || oldFiber; i += 1) {
+    const element = elements[i]
+    let newFiber: Fiber
+
+    const sameType = oldFiber && element.type === oldFiber.type
+    if (sameType) {
+      newFiber = {
+        type: oldFiber!.type,
+        props: element.props,
+        dom: oldFiber!.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      }
+    }
+    if (!sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      }
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = 'DELETION'
+      deletions?.push(oldFiber)
+    }
+    // TODO key check like React
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    // FIXME newFiber!
+    if (!prevSibling) {
+      wipFiber.child = newFiber!
+    } else {
+      prevSibling.sibling = newFiber!
+    }
+
+    prevSibling = newFiber!
+  }
+}
+
 function createDom(fiber: Fiber) {
   // FIXME
   if (fiber.type === 'TEXT') {
@@ -77,31 +112,83 @@ function createDom(fiber: Fiber) {
   }
 
   const dom = document.createElement(fiber.type)
-  const isProperty = (key: string) => key !== 'children'
-
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      // FIXME ts-ignore
-      // @ts-ignore
-      dom[name] = fiber.props[name]
-    })
+  updateDom(dom, { children: [] }, fiber.props)
 
   return dom
 }
 
 function commitRoot() {
+  deletions?.forEach(commitWork)
   if (wipRoot?.child) {
     commitWork(wipRoot.child)
   }
+  currentRoot = wipRoot
   wipRoot = null
 }
 
 function commitWork(fiber: Fiber) {
   const domParent = fiber.parent!.dom! // fiber must have parent.
+
+  // FIXME remove null check
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom !== null) {
+    updateDom(fiber.dom, fiber.alternate!.props, fiber.props)
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom!)
+  }
+
   domParent.appendChild(fiber.dom!)
   if (fiber.child) commitWork(fiber.child)
   if (fiber.sibling) commitWork(fiber.sibling)
+}
+
+const isProperty = (key: string) => key !== 'children' && !isEvent(key)
+const isEvent = (key: string) => key.startsWith('on')
+const isNew = (prev: any, next: any) => (key: string) => prev[key] !== next[key]
+const isGone = (next: { [propName: string]: any }) => (key: string) => !(key in next)
+function updateDom(dom: Fiber['dom'], prevProps: Fiber['props'], nextProps: Fiber['props']) {
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+
+      // FIXME ts-ignore
+      // @ts-ignore
+      dom!.removeEventListener(eventType, prevProps[name])
+    })
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(nextProps))
+    .forEach((name) => {
+      // FIXME ts-ignore
+      // @ts-ignore
+      dom[name] = ''
+    })
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      // FIXME ts-ignore
+      // @ts-ignore
+      dom[name] = nextProps[name]
+    })
+
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+
+      // FIXME ts-ignore
+      // @ts-ignore
+      dom!.addEventListener(eventType, nextProps[name])
+    })
 }
 
 function render(element: VirtualNode, container: HTMLElement) {
@@ -112,7 +199,9 @@ function render(element: VirtualNode, container: HTMLElement) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   }
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
